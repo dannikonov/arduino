12925e0273aaff7517de5271e1b6f47d79907665
Danil Nikonov <danil@seventest.ru>
2024-07-21 16:18:19 +0300

    support layers in cube

--- cube/cube.ino
+++ cube/cube.ino
@@ -1,71 +1,83 @@
 #include <SPI.h>
 
+const int LED_COUNT = 4;
+const int LAYER_COUNT = 4;
+unsigned int leds[LAYER_COUNT];
+
 /**
 74HC595
 Q0 - Q7 - to LED
 SHCP - SCK  - D13
 STCP - SS   - D10
 DS   - MOSI - D11
-
 */
 const byte LATCH = 10;
 
+/*
+Layers:
+0 - D4
+1 - D5
+2 - D6
+3 - D7
+*/
+const unsigned int LAYERS_PINS[LAYER_COUNT] = { 4, 5, 6, 7 };
 
+unsigned long int delayTime = 0;
+unsigned int speed = 200;
+unsigned int count = 0;
 
-const int LED_COUNT = 4;
-bool leds[LED_COUNT][LED_COUNT];
-
-void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-
-  for (int r = 0; r < LED_COUNT; r++) {
-    for (int c = 0; c < LED_COUNT; c++) {
-      leds[r][c] = 0;
-    }
+void initPins() {
+  for (int l = 0; l < LAYER_COUNT; l++) {
+    pinMode(LAYERS_PINS[l], OUTPUT);
+    digitalWrite(LAYERS_PINS[l], LOW);
   }
 
   pinMode(LATCH, OUTPUT);
-  digitalWrite(LATCH, LOW);
-  SPI.transfer(0);
-  digitalWrite(LATCH, HIGH);
 }
-void flushBuffer() {
-  int value = 0;
-  for (int r = LED_COUNT - 1; r >= 0; r--) {
-    for (int c = LED_COUNT - 1; c >= 0; c--) {
-      value |= leds[r][c];
 
-      if (r != 0 || c != 0) {
-        value = value << 1;
-      }
-    }
+void setup() {
+  initPins();
+
+  Serial.begin(9600);
+
+  for (int l = 0; l < LAYER_COUNT; l++) {
+    leds[l] = 0;
   }
 
-  Serial.println(value);
-  SPIWrite(value);
+  SPI.begin();
+  SPIWrite(0);
+
+  delayTime = millis();
+}
+
+void flushBuffer() {
+  for (int l = 0; l < LAYER_COUNT; l++) {
+    SPIWrite(leds[l]);
+
+    PORTD = B00000000;
+    bitSet(PORTD, LAYERS_PINS[l]);
+    delay(1);
+  }
 }
 
 void clearBuffer() {
-  for (int r = LED_COUNT - 1; r >= 0; r--) {
-    for (int c = LED_COUNT - 1; c >= 0; c--) {
-      leds[r][c] = 0;
-    }
+  for (int l = 0; l < LAYER_COUNT; l++) {
+    leds[l] = 0;
   }
 }
 
-void draw_point(int r, int c, bool state) {
-  leds[r][c] = 1;
+void drawVoxel(int l, int r, int c) {
+  leds[l] |= 1 << ((r * LED_COUNT) + c);
 }
 
 // Bresenham's algorithm
-void draw_line(int x0, int y0, int x1, int y1) {
+void drawLine(int l, int x0, int y0, int x1, int y1) {
   int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
   int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
   int err = dx + dy, e2; /* error value e_xy */
 
   for (;;) { /* loop */
-    draw_point(x0, y0, 1);
+    drawVoxel(l, x0, y0);
     if (x0 == x1 && y0 == y1) break;
     e2 = 2 * err;
     if (e2 >= dy) {
@@ -79,11 +91,11 @@ void draw_line(int x0, int y0, int x1, int y1) {
   }
 }
 
-void draw_rectangle(int x0, int y0, int x1, int y1) {
-  draw_line(x0, y0, x1, y0);
-  draw_line(x1, y0, x1, y1);
-  draw_line(x1, y1, x0, y1);
-  draw_line(x0, y1, x0, y0);
+void drawRectangle(int l, int x0, int y0, int x1, int y1) {
+  drawLine(l, x0, y0, x1, y0);
+  drawLine(l, x1, y0, x1, y1);
+  drawLine(l, x1, y1, x0, y1);
+  drawLine(l, x0, y1, x0, y0);
 }
 
 void SPIWrite(int value) {
@@ -94,94 +106,170 @@ void SPIWrite(int value) {
   digitalWrite(LATCH, HIGH);
 }
 
-void rotating_line() {
-  for (int r = 0; r < 2; r++) {
-    for (int i = 0; i < LED_COUNT; i++) {
-      draw_line(0, i, LED_COUNT - 1, LED_COUNT - i - 1);
-      flushBuffer();
-      clearBuffer();
-      delay(200);
-    }
+void rotatingLine(unsigned int layer) {
+  speed = 100;
+  clearBuffer();
 
-    for (int i = LED_COUNT - 2; i > 0; i--) {
-      draw_line(i, 0, LED_COUNT - i - 1, LED_COUNT - 1);
-      flushBuffer();
-      clearBuffer();
-      delay(200);
-    }
+  switch (count) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      drawLine(layer, 0, count, LED_COUNT - 1, LED_COUNT - count - 1);
+      break;
+    case 4:
+      drawLine(layer, 2, 0, 1, LED_COUNT - 1);
+      break;
+    case 5:
+      drawLine(layer, 1, 0, 2, LED_COUNT - 1);
+      break;
+  }
+
+  count++;
+  if (count > 5) {
+    count = 0;
   }
 }
 
-void heartbeat() {
-  for (int i = 0; i < 4; i++) {
-    draw_rectangle(0, 0, LED_COUNT - 1, LED_COUNT - 1);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(1, 1, LED_COUNT - 2, LED_COUNT - 2);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
+void heartbeat(unsigned int layer) {
+  clearBuffer();
+
+  switch (count) {
+    case 0:
+      drawRectangle(layer, 0, 0, LED_COUNT - 1, LED_COUNT - 1);
+      break;
+    case 1:
+      drawRectangle(layer, 1, 1, LED_COUNT - 2, LED_COUNT - 2);
+      break;
+    case 2:
+      drawRectangle(layer, 1, 1, LED_COUNT - 2, LED_COUNT - 2);
+      break;
+    case 3:
+      drawRectangle(layer, 0, 0, LED_COUNT - 1, LED_COUNT - 1);
+      break;
+  }
+
+  count++;
+  if (count == 4) {
+    count = 0;
   }
 }
 
-void random_pixel() {
-  for (int i = 0; i < 80; i++) {
-    int r = random(0, LED_COUNT);
-    int c = random(0, LED_COUNT);
+void randomVoxel() {
+  speed = 50;
 
-    draw_point(r, c, 1);
-    flushBuffer();
-    clearBuffer();
-    delay(40);
+  clearBuffer();
+
+  drawVoxel(
+    random(0, LAYER_COUNT),
+    random(0, LED_COUNT),
+    random(0, LED_COUNT));
+}
+
+void movingRectangle(unsigned int layer) {
+  speed = 100;
+  clearBuffer();
+
+  switch (count) {
+    case 0: 
+    drawRectangle(layer, 0, 0, 2, 2); break;
+    case 1: 
+    drawRectangle(layer, 1, 2, 3, 0); break;
+    case 2: 
+    drawRectangle(layer, 1, 1, 3, 3); break;
+    case 3: 
+    drawRectangle(layer, 0, 1, 2, 3); break;
+  }
+
+    count++;
+  if (count == 4) {
+    count = 0;
   }
 }
 
-void moving_rectangle() {
-  for (int i = 0; i < 3; i++) {
-    draw_rectangle(0, 0, 2, 2);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(1, 2, 3, 0);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(1, 1, 3, 3);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(0, 1, 2, 3);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
+
+void movingRectangle2(unsigned int layer) {
+  speed = 100;
+  clearBuffer();
+
+  switch (count) {
+    case 0: 
+    drawRectangle(layer, 0, 0, 1, 1); break;
+    case 1: 
+    drawRectangle(layer, 2, 0, 3, 1); break;
+    case 2: 
+    drawRectangle(layer, 2, 2, 3, 3); break;
+    case 3: 
+    drawRectangle(layer, 0, 2, 1, 3); break;
+  }
+
+    count++;
+  if (count == 4) {
+    count = 0;
   }
 }
 
-void moving_rectangle2() {
-  for (int i = 0; i < 4; i++) {
-    draw_rectangle(0, 0, 1, 1);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(2, 0, 3, 1);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(2, 2, 3, 3);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-    draw_rectangle(0, 2, 1, 3);
-    flushBuffer();
-    clearBuffer();
-    delay(300);
-  }
-}
 
+unsigned int loopsCounter = 0;
 void loop() {
-  rotating_line();
-  heartbeat();
-  random_pixel();
-  moving_rectangle2();
+
+
+
+  // heartbeat();
+  if ((millis() - delayTime) > speed) {
+    if (loopsCounter < 16) {
+    randomVoxel();
+    
+    } else if (loopsCounter < 22) {
+      rotatingLine(0);
+    } else if (loopsCounter < 28) {
+      rotatingLine(1);
+    } else if (loopsCounter < 34) {
+      rotatingLine(2);
+    } else if (loopsCounter < 40) {
+      rotatingLine(3);
+    
+    } else if (loopsCounter < 44) {
+      movingRectangle(0);
+    } else if (loopsCounter < 48) {
+      movingRectangle(1);
+    } else if (loopsCounter < 52) {
+      movingRectangle(2);  
+    } else if (loopsCounter < 56) {
+      movingRectangle(3);
+    
+
+    } else if (loopsCounter < 60) {
+      movingRectangle2(0);
+    } else if (loopsCounter < 64) {
+      movingRectangle2(1);
+    } else if (loopsCounter < 68) {
+      movingRectangle2(2);  
+    } else if (loopsCounter < 72) {
+      movingRectangle2(3);
+
+    } else if (loopsCounter < 76) {
+      heartbeat(0);
+    } else if (loopsCounter < 80) {
+      heartbeat(1);
+    } else if (loopsCounter < 84) {
+      heartbeat(2);  
+    } else if (loopsCounter < 88) {
+      heartbeat(3);
+    } else {
+      clearBuffer();
+    }
+
+    loopsCounter++;
+
+    if (loopsCounter == 97) {
+      loopsCounter = 0;
+    }
+
+    delayTime = millis();
+  }
+
+
+  flushBuffer();
+  
 }
