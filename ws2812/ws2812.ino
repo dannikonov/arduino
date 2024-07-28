19419c6831e9b5f32a7b8013733fbf50e8deabfc
Danil Nikonov <danil@seventest.ru>
2024-07-28 20:30:40 +0300

    add ir in ws2812

--- ws2812/ws2812.ino
+++ ws2812/ws2812.ino
@@ -1,7 +1,10 @@
 #include <FastLED.h>
+#include <IRremote.h>
 
-#define NUM_LEDS 60
+#define NUM_LEDS 64
+
 #define DATA_PIN 8
+#define IR_RECEIVE_PIN 7
 
 CRGB leds[NUM_LEDS];
 
@@ -9,7 +12,7 @@ volatile int counter = 0;
 volatile uint32_t debounce;
 volatile int n = 0;
 
-DEFINE_GRADIENT_PALETTE(heatmap_gp){
+DEFINE_GRADIENT_PALETTE(heatmap_northern_lights){
   0, 20, 232, 30,     // green 1 #14e81e
   60, 0, 234, 141,    //green 2 #00ea8d
   100, 1, 126, 213,   // blue #017ed5
@@ -17,7 +20,18 @@ DEFINE_GRADIENT_PALETTE(heatmap_gp){
   195, 141, 0, 196,   // purple #8d00c4
   255, 20, 232, 30,   // green 1
 };
-CRGBPalette16 myPal = heatmap_gp;
+CRGBPalette16 northernLigntsPal = heatmap_northern_lights;
+
+DEFINE_GRADIENT_PALETTE(heatmap_dawn){
+  0, 152, 33, 35,     // Vivid Auburn #982123
+  60, 208, 63, 46,    // Persian Red #D03F2E
+  140, 241, 195, 89,  // Crayola's Maize #F1C359
+  195, 148, 194, 128,   // Pistachio #94C280
+  255, 12, 156, 180,   // Blue-Green #0C9CB4
+};
+CRGBPalette16 dawnPal = heatmap_dawn;
+
+int prevCommand = -1;
 
 void setup() {
   Serial.begin(9600);
@@ -31,6 +45,10 @@ void setup() {
   // button D3 - GND
   pinMode(3, INPUT_PULLUP);
   attachInterrupt(1, prev, CHANGE);
+
+  IrReceiver.begin(IR_RECEIVE_PIN);
+  IrReceiver.enableIRIn();
+  IrReceiver.blink13(true);
 }
 
 void next() {
@@ -90,7 +108,19 @@ void program3() {
 int j = 0;
 void northernLights() {
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette(myPal, (i * 4 + j) % 255);
+    leds[i] = ColorFromPalette(northernLigntsPal, (i * 4 + j) % 255);
+  }
+  FastLED.show();
+
+
+  delay(30);
+  j++;
+}
+
+int j = 0;
+void dawn() {
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(dawnPal, (i * 4 + j) % 255);
   }
   FastLED.show();
 
@@ -102,7 +132,62 @@ void northernLights() {
 typedef void (*f)();
 f funcs[4] = { &program1, &program2, &program3, &northernLights };
 
+decode_results results;
+
+void fill(CRGB color) {
+  for (int i = (NUM_LEDS)-1; i >= 0; i--) {
+    leds[i] = color;
+    FastLED.show();
+  }
+}
+
 void loop() {
   // funcs[n]();
-  northernLights();
+  // northernLights();
+
+
+
+
+
+  if (IrReceiver.decode()) {
+    int command = IrReceiver.decodedIRData.command;
+
+
+    Serial.println(command);
+
+    IrReceiver.resume();
+
+    switch (command) {
+      case 22:
+        fill(CRGB::DarkOrange);
+        break;
+      case 12:
+        fill(CRGB::DarkGoldenrod);
+        break;
+      case 24:
+        fill(CRGB::Goldenrod);
+        break;
+      case 94:
+        fill(CRGB::DarkGoldenrod);
+        break;
+      case 8:
+        fill(CRGB::Khaki);
+        break;
+      case 28:
+        fill(CRGB::Maroon);
+        break;
+      case 90:
+        fill(CRGB::MediumSlateBlue);
+        break;
+      case 66:
+        fill(CRGB::Navy);
+        break;
+      case 9:
+        northernLights();
+        break;
+      case 21:
+        dawn();
+        break;
+    }
+  }
 }
